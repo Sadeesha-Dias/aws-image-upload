9e2ee60a87326a3cda7888a8858efa53a92ebc18
SadeeshaNH87 <58145312+SadeeshaNH87@users.noreply.github.com>
2022-12-04 20:53:05 +0530

    Providing AWS accessKey and secretKey from separate files. Removed hardcoded values.

--- src/main/java/com/sadeesha/awsimageupload/Main.java
+++ src/main/java/com/sadeesha/awsimageupload/Main.java
@@ -3,11 +3,38 @@ package com.sadeesha.awsimageupload;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class Main {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
+	}
+
+	//reading the AWS accessKey from a file
+	public String readAccessKey() throws FileNotFoundException {
+		String accKey;
+
+		File accessFile = new File("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\accessKey.txt");
+		Scanner ac = new Scanner(accessFile);
+		ac.useDelimiter("\\Z");
+
+		accKey = ac.next();
+		return accKey;
+	}
+
+	//reading the AWS secretKey from a file
+	public String readSecretKey() throws FileNotFoundException {
+		String secKey;
+
+		File secretFile = new File("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\secretKey.txt");
+		Scanner sc = new Scanner(secretFile);
+		sc.useDelimiter("\\Z");
+
+		secKey = sc.next();
+		return secKey;
 	}
 
 }
--- src/main/java/com/sadeesha/awsimageupload/config/AmazonConfig.java
+++ src/main/java/com/sadeesha/awsimageupload/config/AmazonConfig.java
@@ -5,16 +5,26 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.sadeesha.awsimageupload.Main;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.IOException;
+
 @Configuration
 public class AmazonConfig {
+    public AmazonConfig() throws IOException {
+    }
+    Main mainClass = new Main(); //create an object from main class
+
+    String accKey = mainClass.readAccessKey(); //passing the AWS accessKey
+    String secKey = mainClass.readSecretKey(); //passing the AWS secretKey
+
     @Bean
     public AmazonS3 s3Config(){
         AWSCredentials awsCredentials = new BasicAWSCredentials(
-                "AKIA5KFIS7Z4JBJEUBFC",
-                "vIVObnQLkHusoBA66J8G4A6opTLdPkOdWo4yLktu"
+                accKey,
+                secKey
         );
         return AmazonS3ClientBuilder
                 .standard()
