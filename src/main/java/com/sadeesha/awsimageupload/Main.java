4cc14245797306fcdf30b0145a06a5c810ab14c1
SadeeshaNH87 <58145312+SadeeshaNH87@users.noreply.github.com>
2022-12-05 10:07:58 +0530

    validated security keys file paths.

--- src/main/java/com/sadeesha/awsimageupload/Main.java
+++ src/main/java/com/sadeesha/awsimageupload/Main.java
@@ -14,10 +14,18 @@ public class Main {
 	}
 
 	//reading the AWS accessKey from a file
-	public String readAccessKey() throws FileNotFoundException {
+	public String readAccessKey() throws IOException {
 		String accKey;
+        String accKeyCanonicalPath;
 
 		File accessFile = new File("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\accessKey.txt");
+
+		//validating file path to prevent path traversal vulnerability
+        accKeyCanonicalPath = accessFile.getCanonicalPath();
+        if(!accKeyCanonicalPath.equals("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\accessKey.txt")){
+			System.out.println("Invalid accessKey file: File path is different or the file doesn't exist");
+		}
+
 		Scanner ac = new Scanner(accessFile);
 		ac.useDelimiter("\\Z");
 
@@ -26,10 +34,18 @@ public class Main {
 	}
 
 	//reading the AWS secretKey from a file
-	public String readSecretKey() throws FileNotFoundException {
+	public String readSecretKey() throws IOException {
 		String secKey;
+		String secKeyCanonicalPath;
 
 		File secretFile = new File("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\secretKey.txt");
+
+		//validating file path to prevent path traversal vulnerability
+		secKeyCanonicalPath = secretFile.getCanonicalPath();
+		if(!secKeyCanonicalPath.equals("C:\\Users\\Admin\\Desktop\\Personal_Projects\\Access_keys\\secretKey.txt")){
+			System.out.println("Invalid secretKey file: File path is different or the file doesn't exist");
+		}
+
 		Scanner sc = new Scanner(secretFile);
 		sc.useDelimiter("\\Z");
 
